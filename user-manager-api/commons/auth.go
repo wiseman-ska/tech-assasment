1b1de433fe202a7e60b06e35bd995428fb0c1370
wiseman <wisemanska@gmail.com>
2021-03-16 21:38:07 +0200

    Implemented authorization handler

--- user-manager-api/commons/auth.go
+++ user-manager-api/commons/auth.go
@@ -1,1 +1,97 @@
 package commons
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	privKeyPath = "auth-keys/app.rsa"
+	pubKeyPath  = "auth-keys/app.rsa.pub"
+)
+
+var (
+	verifyKey, signKey []byte
+)
+
+func initKeys() {
+	var err error
+	signKey, err = ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		log.Fatal("[initKeys]: %s\n", err)
+	}
+
+	verifyKey, err = ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		log.Fatal("[initKeys]: %s\n", err)
+		panic(err)
+	}
+}
+
+func GenerateToken(name, role string) (string, error) {
+	Claims := jwt.MapClaims{}
+	Claims["iss"] = "admin"
+	Claims["UserInfo"] = struct {
+		Name string
+		Role string
+	}{name, role}
+
+	Claims["exp"] = time.Now().Add(time.Minute * 20).Unix()
+	t := jwt.NewWithClaims(jwt.GetSigningMethod("RS256"), Claims)
+	tokenString, err := t.SignedString(signKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func Authorize(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	token, err := request.ParseFromRequest(r, request.OAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
+		return verifyKey, nil
+	})
+
+	if err != nil {
+		switch err.(type) {
+
+		case *jwt.ValidationError:
+			vErr := err.(*jwt.ValidationError)
+
+			switch vErr.Errors {
+			case jwt.ValidationErrorExpired:
+				DisplayAppError(w,
+					err,
+					"Access Token is expired, get a new Token",
+					401,
+				)
+				return
+			default:
+				DisplayAppError(w,
+					err,
+					"Error while parsing the Access Token",
+					500,
+				)
+				return
+			}
+		default:
+			DisplayAppError(w,
+				err,
+				"Error while parsing the Access Token",
+				500,
+			)
+			return
+		}
+	}
+	if token.Valid {
+		next(w, r)
+	} else {
+		DisplayAppError(w,
+			err,
+			"Invalid Access Token",
+			401,
+		)
+	}
+}
