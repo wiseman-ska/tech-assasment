e1c7dbbe1f5c56c83af0c8e4c543b103c8c0aefc
wiseman <wisemanska@gmail.com>
2021-03-16 19:10:42 +0200

    Implemented user repository

---
+++ user-manager-api/data/user-repo.go
@@ -0,0 +1,34 @@
+package data
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserRepository struct {
+	Col *mgo.Collection
+}
+
+func (repo *UserRepository) CreateUser(user *models.User) error {
+	hpass, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	user.HashPassword = hpass
+	user.Password = ""
+	err = repo.Col.Insert(&user)
+	return err
+}
+
+func (repo *UserRepository) Login(user models.User) (u models.User, err error) {
+	err = repo.Col.Find(bson.M{"email": user.Email}).One(&u)
+	if err != nil {
+		return
+	}
+	err = bcrypt.CompareHashAndPassword(u.HashPassword, []byte(user.Password))
+	if err != nil {
+		u = models.User{}
+	}
+	return
+}
