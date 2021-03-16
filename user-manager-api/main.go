806d87e2d9f75f7bc61232e656be357034916769
wiseman <wisemanska@gmail.com>
2021-03-16 22:01:37 +0200

    Added application entry point

--- user-manager-api/main.go
+++ user-manager-api/main.go
@@ -1,5 +1,26 @@
 package main
 
+
+import (
+	"log"
+	"net/http"
+	"github.com/urfave/negroni"
+)
+
 func main() {
-	
+	common.StartUp()
+	router := routers.InitRoutes()
+	n := negroni.Classic()
+	n.UseHandler(router)
+	server := &http.Server{
+		Addr: common.AppConf.Server,
+		Handler: n,
+	}
+
+	log.Println("Listening...")
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
+
