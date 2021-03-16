8c6b401c78a4f1dc9825f0fb8526e39d15c1cd37
wiseman <wisemanska@gmail.com>
2021-03-16 21:43:02 +0200

    Included user id variable

--- user-manager-api/controllers/routers/user-router.go
+++ user-manager-api/controllers/routers/user-router.go
@@ -1,18 +1,22 @@
 package routers
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+
+)
 
 func SetupUserRoutes(router *mux.Router) *mux.Router {
 	userRouter := mux.NewRouter()
 	userRouter.HandleFunc("/users/login", controllers.UserLoginHandler).Methods("POST")
 	userRouter.HandleFunc("/users/create", controllers.UserRegisterHandler).Methods("POST")
-	userRouter.HandleFunc("/api/v1/users/update", controllers.UserRegisterHandler).Methods("POST")
-	userRouter.HandleFunc("/api/v1/users/delete", controllers.UserRegisterHandler).Methods("DELETE")
-	userRouter.HandleFunc("/api/v1/users", controllers.UserRegisterHandler).Methods("GET")
+	userRouter.HandleFunc("/api/v1/users/update/{id}", controllers.UserRegisterHandler).Methods("POST")
+	userRouter.HandleFunc("/api/v1/users/delete/{id}", controllers.UserRegisterHandler).Methods("DELETE")
+	userRouter.HandleFunc("/api/v1/users/all", controllers.UserRegisterHandler).Methods("GET")
 	userRouter.HandleFunc("/api/v1/users/{id}", controllers.UserRegisterHandler).Methods("GET")
 
 	router.PathPrefix("/api").Handler(negroni.New(
 		negroni.HandlerFunc(commons.Authorize),
+		
 		negroni.Wrap(userRouter),
 	))
 	return router
