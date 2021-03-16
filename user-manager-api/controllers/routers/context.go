cf9829fe0b1f7c2252bfc95545177d372b70b36d
wiseman <wisemanska@gmail.com>
2021-03-16 21:39:40 +0200

    Updated mongo context

--- user-manager-api/controllers/routers/context.go
+++ user-manager-api/controllers/routers/context.go
@@ -1,1 +1,25 @@
 package routers
+
+import (
+	"gopkg.in/mgo.v2"
+)
+
+type Context struct {
+	MongoDBSession *mgo.Session
+}
+
+func NewContext() *Context {
+	session := commons.GetSession().Copy()
+	context := &Context{
+		MongoDBSession: session,
+	}
+	return context
+}
+
+func (cont *Context) Collection(name string) *mgo.Collection {
+	return cont.MongoDBSession.DB(commons.AppConf.Database).C(name)
+}
+
+func (cont *Context) Close() {
+	cont.MongoDBSession.Close()
+}
