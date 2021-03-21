8b3e082d36fb98de3845eddb1e5ee4e7530428da
wiseman <wisemanska@gmail.com>
2021-03-21 17:29:43 +0200

    Using bson for object ID

--- user-manager-api/models/models.go
+++ user-manager-api/models/models.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
@@ -9,13 +9,13 @@ const (
 )
 
 type User struct {
-	Id              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	FirstName       string             `bson:"firstName" json:"firstName"`
-	LastName        string             `bson:"LastName" json:"lastName"`
-	Email           string             `bson:"email" json:"email"`
-	MobileNumber    string             `bson:"mobileNumber" json:"mobileNumber"`
-	IdNumber        string             `bson:"idNumber" json:"idNumber"`
-	PhysicalAddress string             `bson:"physicalAddress" json:"physicalAddress"`
-	Password        string             `bson:"password,omitempty" json:"password"`
-	HashPassword    []byte             `bson:"hashPassword,omitempty" json:"hashPassword"`
+	Id              bson.ObjectId `bson:"_id,omitempty" json:"id"`
+	FirstName       string        `bson:"firstName" json:"firstName"`
+	LastName        string        `bson:"LastName" json:"lastName"`
+	Email           string        `bson:"email" json:"email"`
+	MobileNumber    string        `bson:"mobileNumber" json:"mobileNumber"`
+	IdNumber        string        `bson:"idNumber" json:"idNumber"`
+	PhysicalAddress string        `bson:"physicalAddress" json:"physicalAddress"`
+	Password        string        `bson:"password,omitempty" json:"password"`
+	HashPassword    []byte        `bson:"hashPassword,omitempty" json:"hashPassword"`
 }
