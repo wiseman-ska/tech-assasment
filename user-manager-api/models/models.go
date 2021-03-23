c6e882a30f6c0550987b7b7a4e15875f8d8d9da4
wiseman <wisemanska@gmail.com>
2021-03-23 09:58:22 +0200

    Fixed last name bug

--- user-manager-api/models/models.go
+++ user-manager-api/models/models.go
@@ -11,7 +11,7 @@ const (
 type User struct {
 	Id              bson.ObjectId `bson:"_id,omitempty" json:"id"`
 	FirstName       string        `bson:"firstName" json:"firstName"`
-	LastName        string        `bson:"LastName" json:"lastName"`
+	LastName        string        `bson:"lastName" json:"lastName"`
 	Email           string        `bson:"email" json:"email"`
 	MobileNumber    string        `bson:"mobileNumber" json:"mobileNumber"`
 	IdNumber        string        `bson:"idNumber" json:"idNumber"`
